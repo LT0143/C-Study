1171d184baaa1c0b4ba131cc5245b1337180590c
LT0143 <1162710759@qq.com>
2017-06-06 22:36:50 +0800

    数据精度

---
+++ CSharpStudy/BinaryConverter.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+   public class BinaryConverter
+    {
+       /// <summary>
+       /// 获取二进制形式的Ulong数据
+       /// </summary>
+       //public static void Main() {
+       //    const int size = 64;
+       //    ulong value;
+       //    char bit;
+
+       //    System.Console.WriteLine("Enter a ninteger:");
+       //    value = ulong.Parse(System.Console.ReadLine());
+       //    ulong mask = 1UL << size - 1;   //1左移63位就是ulong的最大位数除以2.
+       //    System.Console.WriteLine("mask" + mask);
+       //    for (int count = 0; count < size;count ++ )
+       //    {
+       //        bit = ((mask & value) != 0) ? '1' : '0';  //第一次是0111...1&value的二进制
+       //        System.Console.WriteLine(bit);
+       //        mask >>= 1;     //右移除以2
+       //    }
+       //    System.Console.WriteLine();
+       //}
+    }
+
---
+++ CSharpStudy/Num.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// 62页，浮点类型的不准确造成的非预期不相等
+/// </summary>
+   public class Num
+    {
+       public static void Main()
+       {
+           decimal decimalNum = 4.2m;
+           double doubleNum1 = 0.1f * 42f;   //float *float有误差
+           double doubleNum2 = 0.1d * 42d;  
+           float floatNum = 0.1f * 42f;     //float *float有误差  
+
+           Trace.Assert(decimalNum != (decimal)doubleNum1);
+           System.Console.WriteLine("{0} !={1}",decimalNum,(decimal)doubleNum1);
+
+           Trace.Assert((double)decimalNum != doubleNum1);
+           System.Console.WriteLine("{0} !={1}", (double)decimalNum, doubleNum1);
+
+           Trace.Assert((float)decimalNum != floatNum);
+           System.Console.WriteLine("{0} !={1}\n", (float)decimalNum, (float)decimalNum);
+
+           System.Console.WriteLine("decimalNum:{0}  (float)decimalNum:{1}:   (double)decimalNum:{2}\n", decimalNum, (float)decimalNum, (double)decimalNum);
+           System.Console.WriteLine("doubleNum1:{0}  (float)doubleNum1:{1}:   (decimal)decimalNum:{2}\n", doubleNum1, (float)doubleNum1, (decimal)doubleNum1);
+           System.Console.WriteLine("doubleNum2:{0}  (float)doubleNum2:{1}:   (decimal)doubleNum2:{2}\n", doubleNum2, (float)doubleNum2, (decimal)doubleNum2);
+           System.Console.WriteLine("floatNum:{0}  (double)floatNum:{1}:   (decimal)floatNum:{2}\n", floatNum, (double)floatNum, (decimal)floatNum);
+
+       }
+    }
+
