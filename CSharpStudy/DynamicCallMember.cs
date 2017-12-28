e57e6bbebdf146ac72da4cb7d9bd501bf0c941c9
LT0143 <1162710759@qq.com>
2017-12-28 22:04:50 +0800

    新增泛型接口

--- CSharpStudy/DynamicCallMember.cs
+++ CSharpStudy/DynamicCallMember.cs
@@ -9,36 +9,33 @@ using System.Reflection;
 /// </summary>
  public partial  class DynamicCallMember
 {
-     public static void Main(string[] args)
-     {
-         string erroMsg;
-        CommandLineInfo commandLine = new CommandLineInfo();
-         if (!CommandLineHandler.TryParse(args, commandLine, out erroMsg))
-         {
-            Console.WriteLine("erroMsg main"+erroMsg);
-             DisplayHelp();
-         }
-         if (commandLine.Help)
-         {
-             DisplayHelp();
-         }
-         else
-         {
-             if (commandLine.priority != ProcessPriorityClass.Normal)
-             {
-              Console.WriteLine("change thread priority");
-            }
-        }
+     //public static void Main(string[] args)
+     //{
+     //    string erroMsg;
+     //   CommandLineInfo commandLine = new CommandLineInfo();
+     //    if (!CommandLineHandler.TryParse(args, commandLine, out erroMsg))
+     //    {
+     //       Console.WriteLine("erroMsg main"+erroMsg);
+     //        DisplayHelp();
+     //    }
+     //    if (commandLine.Help)
+     //    {
+     //        DisplayHelp();
+     //    }
+     //    else
+     //    {
+     //        if (commandLine.priority != ProcessPriorityClass.Normal)
+     //        {
+     //         Console.WriteLine("change thread priority");
+     //       }
+     //   }
         
-    }
+    //}
 
     private static void  DisplayHelp()
     {
         Console.WriteLine("DisplayHelp");
     }
-
-  
-  
 
 }
 
---
+++ CSharpStudy/Ipair.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 314页，使用多个类型参数声明泛型
+/// </summary>
+/// <typeparam name="TFirsst"></typeparam>
+/// <typeparam name="TSecond"></typeparam>
+interface IPair<TFirsst,TSecond>
+{
+    TFirsst First { get; set; }
+    TSecond Second { get; set; }
+
+}
+
+interface IPair<T>
+{
+    T First { get; set; }
+    T Second { get; set; }
+}
+
+public struct Pair<T> : IPair<T>
+{
+    public Pair(T first)
+    {
+        _First = first;
+        _Second = default(T);
+    }
+    private T _First;
+
+    public T First
+    {
+        get { return _First; }
+        set { _First = value; }
+    }
+    private T _Second;
+
+    public T Second
+    {
+        get { return _Second; }
+        set { _Second = value; }
+    }
+}
+
+public struct Pair<TFirst, TSecond> : IPair<TFirst, TSecond>
+{
+    public Pair(TFirst first, TSecond second)
+    {
+        _First = first;
+        _Second = second;
+    }
+
+    private TFirst _First;
+
+    public TFirst First
+    {
+        get { return _First; }
+        set { _First = value; }
+    }
+    private TSecond _Second;
+
+    public TSecond Second
+    {
+        get { return _Second; }
+        set { _Second = value; }
+    }
+
+}
+
+public class PairTest
+{
+    public static void Main(string[] args)
+    {
+        Pair<int, string> historicalEvent = new Pair<int, string>(1914, "what ....");
+        Console.WriteLine("{0},{1}", historicalEvent.First, historicalEvent.Second);
+    }
+
+}
