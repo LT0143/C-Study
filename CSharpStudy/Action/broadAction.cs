4223c3de106f5f307afb027c6d93e0ed4ac161d1
LT0143 <1162710759@qq.com>
2018-01-11 11:51:01 +0800

    action

---
+++ CSharpStudy/Action/broadAction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class broadAction
+{
+  static  Action<object> objAction =
+(object data) =>
+{
+    Console.WriteLine(data);
+};
+  static  Action<string> strActino = objAction;
+
+  static  Func<string> strFunc = () => Console.ReadLine();
+
+    Func<object> objFunc = strFunc;
+
+    public static void Main()
+    {
+
+        objAction(121);
+
+        Console.WriteLine();
+
+
+    }
+ 
+}
+ 
--- CSharpStudy/T/Order.cs
+++ CSharpStudy/T/Order.cs
@@ -27,21 +27,21 @@ public class OrderFactory : IEntityFactory<Guid, Order>
 class Genericity
 {
     //测试入口
-    public static void Main()
-    {
-        Guid guid = new Guid("6F9619FF-8B86-D011-B42D-00C04FC964FF");
-        Order od = new Order(guid);
-        OrderFactory fc = new OrderFactory();
-        fc.creatnew(guid);
-        EntityDictionnary<Guid, Order, OrderFactory> entity = new EntityDictionnary<Guid, Order, OrderFactory>();
+    //public static void Main()
+    //{
+    //    Guid guid = new Guid("6F9619FF-8B86-D011-B42D-00C04FC964FF");
+    //    Order od = new Order(guid);
+    //    OrderFactory fc = new OrderFactory();
+    //    fc.creatnew(guid);
+    //    EntityDictionnary<Guid, Order, OrderFactory> entity = new EntityDictionnary<Guid, Order, OrderFactory>();
 
-        string key = entity.Count.ToString();
-        //string value = entity[0].vale.ToString();
+    //    string key = entity.Count.ToString();
+    //    //string value = entity[0].vale.ToString();
 
-        Console.WriteLine();
+    //    Console.WriteLine();
 
         
-    }
+    //}
     
 }
  
